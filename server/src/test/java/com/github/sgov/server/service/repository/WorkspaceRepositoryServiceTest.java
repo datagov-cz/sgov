1c6898569e850dd9b8256e1cd97658ceeee67b55
Miroslav Blasko <blcham@gmail.com>
2020-05-27 00:09:36 +0200

    [New] Add workspace repository service test for creating vocabulary context

--- server/src/test/java/com/github/sgov/server/service/repository/WorkspaceRepositoryServiceTest.java
+++ server/src/test/java/com/github/sgov/server/service/repository/WorkspaceRepositoryServiceTest.java
@@ -1,42 +1,54 @@
 package com.github.sgov.server.service.repository;
 
-import com.github.sgov.server.dao.WorkspaceDao;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import com.github.sgov.server.environment.Environment;
+import com.github.sgov.server.environment.Generator;
+import com.github.sgov.server.model.UserAccount;
+import com.github.sgov.server.model.Workspace;
 import com.github.sgov.server.service.BaseServiceTestRunner;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.Assert;
-import org.junit.Ignore;
+import cz.cvut.kbss.jopa.model.EntityManager;
+import java.net.URI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class,
-    classes = {WorkspaceRepositoryService.class})
-class WorkspaceRepositoryServiceTest  extends BaseServiceTestRunner {
+class WorkspaceRepositoryServiceTest extends BaseServiceTestRunner {
+
+    @Autowired
+    private EntityManager em;
 
     @Autowired
     private WorkspaceRepositoryService sut;
 
-    @Mock
-    private WorkspaceDao workspaceDao;
+    private UserAccount user;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+        this.user = Generator.generateUserAccountWithPassword();
+        transactional(() -> em.persist(user));
+        Environment.setCurrentUser(user);
     }
 
-    // TODO
-    void getAllWorkspaceIrisReturnsAllIris() {
-        final List<String> iris = new ArrayList<>();
-        iris.add("http://example.org");
-        iris.add("http://example2.org");
-        Mockito.when(workspaceDao.getAllWorkspaceIris()).thenReturn(iris);
-        Assert.assertEquals(iris, sut.getAllWorkspaceIris());
+    @Test
+    void createVocabularyContextUpdatesWorkspace() {
+        final Workspace workspace = Generator.generateWorkspace();
+        transactional(() -> em.persist(workspace));
+
+        sut.createVocabularyContext(
+            workspace.getUri(),
+            URI.create("http://example.org/test-vocabulary-1.0.0"),
+            true);
+
+        final Workspace result = em.find(Workspace.class, workspace.getUri());
+        assertNotNull(result);
+        assertEquals(user.toUser(), result.getAuthor());
+        assertNotNull(result.getCreated());
+        assertEquals(1, result.getVocabularyContexts().size());
+        assertNotNull(result.getVocabularyContexts().iterator().next().getChangeTrackingContext());
     }
 }
-
