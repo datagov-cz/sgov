999b741f62a14f40648094c4887a78fde71c5d38
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-06-09 16:34:43 +0200

    [Fix] Fix vocabulary w.r.t. the updates to popis dat ontology IRI.

--- server/src/main/java/com/github/sgov/server/util/Vocabulary.java
+++ server/src/main/java/com/github/sgov/server/util/Vocabulary.java
@@ -6,68 +6,59 @@ package com.github.sgov.server.util;
 @SuppressWarnings("checkstyle:ConstantName")
 public final class Vocabulary {
 
-    public static final String s_c_metadatovy_kontext =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "metadatový-kontext";
-    public static final String s_c_slovnikovy_kontext =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "slovníkovy-kontext";
+    private static final String WORKSPACE_NAMESPACE = "https://slovník.gov.cz/datový/pracovní-prostor/pojem/";
+    private static final String DATA_DESCRIPTION_NAMESPACE = "http://onto.fel.cvut.cz/ontologies/slovník/agendový/popis-dat/pojem/";
+
+    public static final String s_c_metadatovy_kontext = WORKSPACE_NAMESPACE + "metadatový-kontext";
+    public static final String s_c_slovnikovy_kontext = WORKSPACE_NAMESPACE + "slovníkovy-kontext";
     public static final String s_c_slovnikovy_kontext_pouze_pro_cteni =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "slovníkový-kontext-pouze-pro-čtení";
-    public static final String s_c_kontext_sledovani_zmen =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "kontext-sledování-změn";
+            WORKSPACE_NAMESPACE + "slovníkový-kontext-pouze-pro-čtení";
+    public static final String s_c_kontext_sledovani_zmen = WORKSPACE_NAMESPACE
+            + "kontext-sledování-změn";
 
-    public static final String s_p_odkazuje_na_kontext =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "odkazuje-na-kontext";
-    public static final String s_p_vychazi_z_verze =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "vychází-z-verze";
+    public static final String s_p_odkazuje_na_kontext = WORKSPACE_NAMESPACE
+            + "odkazuje-na-kontext";
+    public static final String s_p_vychazi_z_verze = WORKSPACE_NAMESPACE + "vychází-z-verze";
     public static final String s_p_ma_pracovni_metadatovy_kontext =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "má-pracovní-metadatový-kontext";
-    public static final String s_p_obsahuje_slovnik =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "obsahuje-slovník";
-    public static final String s_p_meni_verzi =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "mění-verzi";
+            WORKSPACE_NAMESPACE + "má-pracovní-metadatový-kontext";
+    public static final String s_p_obsahuje_slovnik = WORKSPACE_NAMESPACE
+            + "obsahuje-slovník";
+    public static final String s_p_meni_verzi = WORKSPACE_NAMESPACE + "mění-verzi";
     public static final String s_p_je_technickym_kontextem_slovniku =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "je-technickým-kontextem-slovníku";
-    public static final String s_p_ma_kontext_sledovani_zmen =
-            "https://slovník.gov.cz/datový/pracovní-prostor/pojem/" + "má-kontext-sledování-změn";
+            WORKSPACE_NAMESPACE + "je-technickým-kontextem-slovníku";
+    public static final String s_p_ma_kontext_sledovani_zmen = WORKSPACE_NAMESPACE
+            + "má-kontext-sledování-změn";
 
 
-    public static final String s_c_administrator =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/" + "administrátor";
-    public static final String s_c_uzivatel =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/" + "uživatel";
-    public static final String s_c_omezeny_uzivatel =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/" + "omezený-uživatel";
-    public static final String s_c_uzamceny_uzivatel =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/"
-                    + "uzamčený-uživatel";
-    public static final String s_c_zablokovany_uzivatel =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/"
-                    + "zablokovaný-uživatel";
-    public static final String s_p_ma_puvodni_heslo =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/" + "má-původní-heslo";
+    public static final String s_c_administrator = DATA_DESCRIPTION_NAMESPACE
+            + "administrátor";
+    public static final String s_c_uzivatel = DATA_DESCRIPTION_NAMESPACE
+            + "uživatel";
+    public static final String s_c_omezeny_uzivatel = DATA_DESCRIPTION_NAMESPACE
+            + "omezený-uživatel";
+    public static final String s_c_uzamceny_uzivatel = DATA_DESCRIPTION_NAMESPACE
+            + "uzamčený-uživatel";
+    public static final String s_c_zablokovany_uzivatel = DATA_DESCRIPTION_NAMESPACE
+            + "zablokovaný-uživatel";
+    public static final String s_p_ma_puvodni_heslo = DATA_DESCRIPTION_NAMESPACE
+            + "má-původní-heslo";
 
-    public static final String s_p_ma_heslo =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/" + "má-heslo";
-    public static final String s_p_ma_krestni_jmeno =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/" + "má-křestní-jméno";
-    public static final String s_p_ma_prijmeni =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/" + "má-příjmení";
-    public static final String s_p_ma_uzivatelske_jmeno =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/"
-                    + "má-uživatelské-jméno";
+    public static final String s_p_ma_heslo = DATA_DESCRIPTION_NAMESPACE
+            + "má-heslo";
+    public static final String s_p_ma_krestni_jmeno = DATA_DESCRIPTION_NAMESPACE
+            + "má-křestní-jméno";
+    public static final String s_p_ma_prijmeni = DATA_DESCRIPTION_NAMESPACE
+            + "má-příjmení";
+    public static final String s_p_ma_uzivatelske_jmeno = DATA_DESCRIPTION_NAMESPACE
+            + "má-uživatelské-jméno";
 
     public static final String s_p_ma_datum_a_cas_posledni_modifikace =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/"
-                    + "má-datum-a-čas-poslední-modifikace";
-    public static final String s_p_ma_posledniho_editora =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/"
-                    + "má-posledního-editora";
-    public static final String s_p_ma_datum_a_cas_vytvoreni =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/"
-                    + "má-datum-a-čas-vytvoření";
-    public static final String s_p_ma_autora =
-            "http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/"
-                    + "má-autora";
+            DATA_DESCRIPTION_NAMESPACE + "má-datum-a-čas-poslední-modifikace";
+    public static final String s_p_ma_posledniho_editora = DATA_DESCRIPTION_NAMESPACE
+            + "má-posledního-editora";
+    public static final String s_p_ma_datum_a_cas_vytvoreni = DATA_DESCRIPTION_NAMESPACE
+            + "má-datum-a-čas-vytvoření";
+    public static final String s_p_ma_autora = DATA_DESCRIPTION_NAMESPACE + "má-autora";
 
     private Vocabulary() {
     }
